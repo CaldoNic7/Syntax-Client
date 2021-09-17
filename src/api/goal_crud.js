f43d3f2fe017ef7a1ba8a48c6f2f143db2eb7081
CaldoNic7 <caldwellnicholaslv@gmail.com>
2021-09-17 00:05:59 -0400

    Add index, show and update functionality
    got index, show and update functionality working. Going to work on push this branch up to the dev branch and then work on delete inside of a crud branch. I really need to get better about version control.

--- src/api/goal_crud.js
+++ src/api/goal_crud.js
@@ -21,3 +21,44 @@ export const createGoal = (user, goalData) => {
     }
   })
 }
+
+export const indexUserGoals = user => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/goals/',
+    headers: {
+      Authorization: `TOKEN ${user.token}`
+    }
+  })
+}
+
+export const showGoal = (user, goalId) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/goals/' + goalId,
+    headers: {
+      Authorization: `TOKEN ${user.token}`
+    }
+  })
+}
+
+export const updateGoal = (user, goalId, goalData) => {
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + `/goals/${goalId}/`,
+    headers: {
+      Authorization: `TOKEN ${user.token}`
+    },
+    data: {
+      goal: {
+        name: goalData.name,
+        chars_per_min: goalData.chars_per_min,
+        language: goalData.language,
+        target_date: goalData.target_date,
+        practice_num: goalData.practice_num,
+        measurement: goalData.measurement,
+        frequency: goalData.frequency
+      }
+    }
+  })
+}
